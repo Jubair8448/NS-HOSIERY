67caec94b988bc9c99e30ee0f18c4aabcefe0967
mohdjubair <22bcs50172@cuchd.in>
2025-12-19 15:00:01 +0530

    fix: added papaparse types

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 import withNextIntl from 'next-intl/plugin'
 
 const nextConfig: NextConfig = withNextIntl()({
-  /* config options here */
   images: {
     remotePatterns: [
       {
