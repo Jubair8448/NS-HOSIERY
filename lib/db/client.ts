6801b6f9844df31be6a23b5d7269dc2d1f59e442
mohdjubair <22bcs50172@cuchd.in>
2025-12-19 14:03:26 +0530

    fix: videos type mismatch

--- lib/db/client.ts
+++ lib/db/client.ts
@@ -19,8 +19,6 @@ const options = {
 let client: MongoClient
 
 if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   const globalWithMongo = global as typeof globalThis & {
     _mongoClient?: MongoClient
   }
@@ -30,16 +28,14 @@ if (process.env.NODE_ENV === 'development') {
   }
   client = globalWithMongo._mongoClient
 } else {
-  // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
 }
 
-// Export a module-scoped MongoClient. By doing this in a
-// separate module, the client can be shared across functions.
 export default client
+
 export interface IProduct extends Document, IProductInput {
   _id: string
   createdAt: Date
   updatedAt: Date
-  videos?: string[]
+  videos: string[]   // FIXED HERE
 }
